01b503eb8a5025be4533e31fface71af37e2edf8
Laura Lospitao <lauralospitao@gmail.com>
2021-07-22 11:09:56 +0200

    #3 Registration>When the user clicks on "Registro" button, API Registration Service is called

--- public/JS/register_user.js
+++ public/JS/register_user.js
@@ -1,37 +1,38 @@
-
-$(document).ready(function() {
-
-
-
-    let registerUserWebService = API_DOMAIN + '/v1/users';
+function createNewUser(username, email, password, confirmation_password, registerUserWebService) {
 
 
 
-    $('button#register').onclick(function() {
-        let username = $('#username').val();
-        let email = $('#email').val();
-        let password = $('#password').val();
-        let confirmation_password = $('#confirmation_password').val();
+    $.ajax({
+        url : registerUserWebService,
+        data : {
+            'username' : username,
+            'email' : email,
+            'password' : username,
+            'confirmation_password' : confirmation_password,
+        },
+        type : 'POST',
+        dataType : 'json',
+        success: function (data) {
+            console.log('Usuario registrado con éxito.');
+            console.log(data)
 
-        $.ajax({
-            url : registerUserWebService,
-            data : {
-                'username' : username,
-                'email' : email,
-                'password' : username,
-                'confirmation_password' : confirmation_password,
-            },
-            type : 'POST',
-            dataType : 'json',
-            success: function (data) {
-                console.log('Submission was successful.');
-                console.log(data)
+        },
+        error: function (data) {
+            console.log('Se ha producido un error al registrar al usuario.');
+        },
+    })
+}
 
-            },
-            error: function (data) {
-                console.log('An error occurred.');
-                console.log(data['stageUuidDevuelto']);
-            },
-        })
+    function createNewUserEvent() {
+        $('button#register').onclick(function() {
+            let username = $('#username').val();
+            let email = $('#email').val();
+            let password = $('#password').val();
+            let confirmation_password = $('#confirmation_password').val();
+            let registerUserWebService = API_DOMAIN + '/v1/users';
+            createNewUser(username, email, password, confirmation_password, registerUserWebService);
+        });
+    }
+    $(document).ready(function() {
+        createNewUserEvent();
     });
-});
